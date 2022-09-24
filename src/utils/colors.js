e5f62d9572f2294146eb36b02ca92532de954139
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-24 05:08:15 +0100

    created the rest of colors object and also created typography contents in typography.js

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -7,3 +7,24 @@ export const colors = {
     600 : "#355691",
     700 : "#90DDF0"
 }
+
+export const red = {
+    100 : "#BF3100",
+    200 : "#DB2955",
+    300 : "#B98389",
+    400 : "#EE2E31"
+}
+
+export const green = {
+    100 : "#1D7874",
+    200 : "#071E22",
+    300 : "#80DED9",
+    400 : "#119DA4"
+}
+
+export const neutral = {
+    100 : "#D8DAD3",
+    200 : "#F1F2EB",
+    300 : "#CFDEE7",
+    400 : "#F3F8F2"
+}
