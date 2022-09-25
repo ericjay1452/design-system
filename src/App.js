0c70cb88b28e0aa878444cd5f5e619fdc0fd41b6
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-25 09:42:30 +0100

    Abou to setup by design to use darkMode, by importing ThemeProvider from styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { PrimaryButton } from "./components/Button/Button";
 import { SecondaryButton } from "./components/Button/Button";
 import { TertiaryButton } from "./components/Button/Button";
-import { GlobalStyles } from './utils';
+import { GlobalStyles, darkTheme, defaultTheme } from './utils';
 
 // P.S
 
 function App() {
+  const [darkMode, setDarkMode ] = useState(false)
+  
   return (
     <>
 <PrimaryButton modifiers = {["small"]}>Hi</PrimaryButton>
--- src/utils/themes.js
+++ src/utils/themes.js
@@ -11,3 +11,21 @@ export const defaultTheme = {
     PrimaryFont :PrimaryFont
 }
 
+export const darkTheme = {
+    primaryColor : blue[300],
+    primaryHoverColor : blue[200],
+    primaryActiveColor : blue[100],
+    textColorOnPrimary : neutral[100],
+    textColor : neutral[400],
+    textColorInverted : neutral[100],
+    PrimaryFont :PrimaryFont,
+    
+    backgroundColors : {
+        light : neutral[100],
+        lightDark : neutral[400],
+        dark : "#000",
+
+    }
+
+}
+
